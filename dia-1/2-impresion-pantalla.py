7eda37772ff319cef8ccc96511fff163363d4601
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-10-10 21:22:26 -0500

    agregacion: agrege los archivos iniciales de python

---
+++ dia-1/2-impresion-pantalla.py
@@ -0,0 +1,18 @@
+correo = 'ederiveroman@gmail.com'
+estatura = 1.89
+print(correo)
+
+# juntar dos textos, al momento de poner la coma se agregar un espacio al imprimir
+print('El correo del usuario es',correo)
+
+# si usamos el mas (+) concatenara los textos PERO sin agregar el espacio
+print('El correo del usuario es '+correo)
+
+# la concatenacion por el mas (+) solamente se hace entre strings 
+print('La estatura del usuario es',estatura)
+
+# parsear > convertir un tipo de dato a otro
+print('La estatura del usuario es '+str(estatura))
+# str(var) > convierte el tipo de dato del proveniente de la variable a un string
+# int(var) > convertira a entero
+# float(var) > convertira a float ...
