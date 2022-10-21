77ed2b751bac479fa84e2bac24b96cc5fda54d45
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-10-20 21:13:48 -0500

    repaso funciones

--- Dia-2/3-funciones.py
+++ Dia-2/3-funciones.py
@@ -27,5 +27,39 @@ print(resultado)
 resultado = sumar_n_numeros(10,5,18,7,22,56,89,15,14,13,15,18,19,14,17,20)
 print(resultado)
 
-def inscribir_personas(persona):
-    print(persona)
+def filtro(**kwargs):
+    # kwargs > keyword argument > recibiremos un numero ilimitado de parametros pero utilizando el nombre del parametro y su valor
+    print(kwargs)
+    for llave in kwargs.keys():
+        # print(llave)
+        valor = kwargs.get(llave)
+        print(llave,':', valor)
+
+filtro(nombre='eduardo', edad=30, sexo='M')
+
+filtro(nombre='eduardo', sexo='M',nacionalidad='PERUANO')
+
+
+
+curso = {
+    'nombre':'Matematica',
+    'dificultad': 'Intermedio',
+    'experiencia': 'Ninguna',
+    0: 'hola'
+}
+
+print(curso)
+print(curso.keys())
+print(curso.values())
+print(curso[0])
+print(curso['dificultad'])
+print(curso.get('calificacion'))
+print(curso.get('nombre', 'No Hay'))
+# modificar valores en mi diccionario, si esa llave no existe, entonces se creara
+curso['mas_info'] = 'esta es una informacion adicional'
+
+# metodo .get SOLAMENTE sirve para visualizar la informacion, mas no para asignacion
+# curso.get('otra_info') = 'esta es otra informacion'
+
+
+
