a2fe495178da2f336f4fc42b14a0e326a2248235
Paolo <paholo2054@gmail.com>
2022-10-28 20:50:33 -0500

    estructura basica de mi proyecto en flask

--- Dia-3/app/__init__.py
+++ Dia-3/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -19,4 +19,23 @@ usuarios = [
 
 @app.route("/<dni>")
 def home(dni):
-  return f'Hola tu numero de dni es: {dni}'
+  for usuario in usuarios:
+    if usuario['dni'] == dni:
+      return f'El nombre del usuario es: {usuario["nombre"]}'
+  return f'El usuario con el dni: {dni} no fue encontrado'
+
+@app.route("/auth/login", methods=['POST'])
+def login():
+  usuario = {
+    "username": "paolo",
+    "password": "osito123"
+  }
+  json = request.get_json()
+  if (usuario['username']==json['username'] and usuario['password']==json['password']):
+    return 'Usuario logeado'
+  return 'Las credenciales son incorrectas'
+
+
+@app.route("/products", methods=['POST', 'GET', 'PUT', 'DELETE'])
+def products():
+  return 'Esta es la ruta productos'
