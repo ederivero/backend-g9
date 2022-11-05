77abfd88e65a727013c4b0b922e13d1938efb024
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-11-04 19:07:17 -0500

    agrege formulario de agregar alumno

---
+++ Dia-2/static/js/agregar_alumno.js
@@ -0,0 +1,28 @@
+const agregar = document.getElementById("btn_agregar");
+const nombre = document.getElementById("nombre");
+const ape_paterno = document.getElementById("ape_paterno");
+const ape_materno = document.getElementById("ape_materno");
+const correo = document.getElementById("correo");
+const num_emergencia = document.getElementById("num_emergencia");
+
+const agregarEvento = (e) => {
+  e.preventDefault();
+  const data = {
+    nombre: nombre.value,
+    ape_materno: ape_materno.value,
+    ape_materno: ape_materno.value,
+    correo: correo.value,
+    num_emergencia: num_emergencia.value,
+  };
+
+  fetch("/agregar-alumno", { method: "POST", body: JSON.stringify(data) })
+    .then((request) => {
+      return request.json();
+    })
+    .then((data) => {
+      console.log(data);
+      window.location.href = "/mostrar-alumnos";
+    });
+};
+
+agregar.addEventListener("click", agregarEvento);
