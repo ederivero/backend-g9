eae50f2e225536d196249fbba3c589bf1c45ae70
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-12-01 20:01:12 -0500

    avance nodejs

---
+++ mi_primer_proyecto/info-adicional.js
@@ -0,0 +1,2 @@
+const nombre = "eduardo";
+const edad = 30;
---
+++ mi_primer_proyecto/practica.js
@@ -0,0 +1,7 @@
+// import isodd from "is-odd";
+const isodd = require("is-odd");
+const esPar = isodd(15);
+
+console.log("hola");
+
+console.log(esPar);
