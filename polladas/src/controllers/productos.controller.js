83dfa0e995a65dab2a4e731288cbfd3460632cf0
Paolo Soncco <paholo2054@gmail.com>
2022-12-09 21:08:21 -0500

    express with prisma

--- polladas/src/controllers/productos.controller.js
+++ polladas/src/controllers/productos.controller.js
@@ -21,9 +21,92 @@ export const crearProducto = async (req, res) => {
       content: nuevoProducto,
     });
   } catch (error) {
-    console.log("Error!!!!!");
     res.status(400).json({
       message: "Error al crear el producto",
+      error: error.message,
+    });
+  }
+};
+
+export const listarProductos = async (req, res) => {
+  try {
+    const productos = await Prisma.producto.findMany();
+    return res.status(200).json({
+      message: "Productos encontrados",
+      content: productos,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: error.message,
+    });
+  }
+};
+
+export const traerProductoPorId = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const producto = await Prisma.producto.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!producto) {
+      return res.status(404).json({
+        message: "Producto no encontrado",
+      });
+    }
+    return res.status(200).json({
+      message: "Producto encontrado",
+      content: producto,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: error.message,
+    });
+  }
+};
+
+export const actualizarProducto = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  try {
+    const findProduct = await Prisma.producto.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!findProduct) {
+      return res.status(404).json({
+        message: "Producto no encontrado",
+      });
+    }
+    const producto = await Prisma.producto.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        nombre: data.nombre,
+        cantidad: data.cantidad,
+        precio: data.precio,
+        disponibilidad: data.disponibilidad,
+      },
+      select: {
+        nombre: true,
+        cantidad: true,
+        precio: true,
+      },
+    });
+
+    return res.status(201).json({
+      message: "Producto actualizado",
+      content: producto,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error en el servidor",
+      error: error.message,
     });
   }
 };
--- polladas/src/routes/productos.routes.js
+++ polladas/src/routes/productos.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { crearProducto } from "../controllers/productos.controller.js";
+import { actualizarProducto, crearProducto, listarProductos, traerProductoPorId } from "../controllers/productos.controller.js";
 
 export const productosRouter = Router();
 
 productosRouter.post("/producto", crearProducto);
+productosRouter.get("/producto", listarProductos);
+productosRouter.get("/producto/:id", traerProductoPorId);
+productosRouter.put("/producto/:id", actualizarProducto);
