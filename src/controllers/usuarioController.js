cc084e91576abe1f735eca3403230497f39890b2
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-12-14 21:57:46 -0500

    registro de usuarios

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,10 +1,20 @@
 import { Usuario } from "../models/usuarioModel.js";
 
-export function registro(req, res) {
+export async function registro(req, res) {
   const data = req.body;
   console.log(data);
 
-  res.status(201).json({
-    message: "Usuario creado exitosamente",
-  });
+  try {
+    const nuevoUsuario = await Usuario.create(data);
+    console.log(nuevoUsuario);
+
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error al crear el usuario",
+      content: error.message,
+    });
+  }
 }
